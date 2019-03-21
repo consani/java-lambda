dbe81e1fc0233833077fcb62b1f94be46cad1cc8
consani <braulio.consani@gmail.com>
2019-03-21 19:49:35 -0300

    Add files via upload

---
+++ Operacao.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Operacao {
+	public double executar(double num1, double num2);
+}
---
+++ Soma.java
@@ -0,0 +1,10 @@
+public class Soma implements Operacao{
+
+	@Override
+	public double executar(double num1, double num2) {
+		return num1+num2;
+	}
+
+	
+	
+}
